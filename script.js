33b7df0c831ae3b8f9e5e3d80e0317f7596664b2
miiifo <osato.0427@outlook.jp>
2022-11-13 13:56:47 +0900

    Add buttons and functionality

--- script.js
+++ script.js
@@ -1,47 +1,130 @@
 let container=document.querySelector("#container");
-    
+let colorPicker=document.querySelector(".color-picker");
+let bgcolorPicker=document.querySelector(".bgcolor-picker");
+let range=document.querySelector("#range");
+let currentValue=document.querySelector("#current-value");
+let rainbow=document.querySelector(".rainbow")
+let shading=document.querySelector(".shading")
+let eraser=document.querySelector(".eraser")
+let clear=document.querySelector(".clear")
+let colorMode ="normal";
+let gridColor="#000000";
+let isShading=false;
+let colorDict={"normal":getGridColor, "rainbow":makeRainbow, "shading":shade, "eraser":getGridColor}
+let isDown = false;
+
+
 createGrid(16);
+
+
+
+range.addEventListener("mouseup",()=>{
+    clearGrid();
+    createGrid(range.value);
+});
+
+
+colorPicker.addEventListener("change",(e)=>{
+    gridColor=e.target.value;
+    colorMode="normal";
+});
+
+
+bgcolorPicker.addEventListener("change",(e)=>{
+    let grid=document.querySelectorAll(".grid-element")    
+    grid.forEach(newBg=>{
+        newBg.style.backgroundColor=e.target.value;
+    });
+});
+
+
+rainbow.addEventListener("click",(e)=>{
+    colorMode="rainbow";
+});
+
+
+shading.addEventListener("click",(e)=>{
+    isShading = !isShading
+    colorMode="shading";
+});
+
+
+eraser.addEventListener("click",(e)=>{
+    let grid=document.querySelector(".grid-element");
+    isShading=false;
+    
+    gridColor=grid.style.backgroundColor;
+    colorMode="eraser";
+});
+
+
+range.addEventListener("mousemove",()=>{
+    currentValue.innerHTML=`${range.value}×${range.value}`;
+});
+
+
+clear.addEventListener("click",()=>{
+    let grid=document.querySelectorAll(".grid-element")    
+    grid.forEach(newBg=>{
+        newBg.style.backgroundColor=bgcolorPicker.value;
+        newBg.style.opacity=1;
+    });
+});
+
+container.addEventListener("mouseup", function(){
+    isDown =false;
+});
+
 
 function createGrid(row){
     container.style.gridTemplateColumns=`repeat(${row}, 1fr)`;
     container.style.gridTemplateRows=`repeat(${row}, 1fr)`;
-    for(let i=0; i<row; i++){
-        for(let v=0; v<row; v++){
-            let grid=document.createElement("div");
-            container.appendChild(grid);
-            grid.classList.add('grid-element');
-            grid.addEventListener("mousedown", function(){
-                
-                //Change the color to rainbow
-                /*const r = Math.floor(Math.random() * 256);
-                const g = Math.floor(Math.random() * 256);
-                const b = Math.floor(Math.random() * 256);
-                grid.style.backgroundColor=`rgb(${r},${g},${b})`;*/
-                
-                //Add a 10% of black each pass
-                grid.style.backgroundColor=`rgb(0,0,0)`;
-                let currentOpacity=Number(grid.style.opacity);
-                grid.style.opacity=currentOpacity+.1;
-            });
-
-        }
     
+    for(let i=0; i<row*row; i++){
+        let grid=document.createElement("div");
+        container.appendChild(grid);
+        grid.classList.add('grid-element');
+        grid.style.backgroundColor=bgcolorPicker.value;
+        grid.style.opacity=1;
+        grid.addEventListener("mousedown", function(){
+            isDown =true;
+            grid.style.backgroundColor=colorDict[colorMode]();
+        });
+        grid.addEventListener("mouseover", function(){
+            if(isDown){
+                grid.style.backgroundColor=colorDict[colorMode]();
+                if(isShading){
+                    decreaseOpacity(grid);
+                }else{
+                    grid.style.opacity=1;
+                }
+            }
+        });
     } 
 }
 
-let range=document.querySelector("#range");
-range.addEventListener("click",()=>{
-    let number=prompt("set the number of squares per side (2 to 100)");
-    if (number>=2 &&number<=100){
-        clearGrid();
-        createGrid(number);
-    }else {
-            location.reload();
-        }
-})
+function makeRainbow(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    gridColor=`rgb(${r},${g},${b})`;
+    return gridColor;
+}
+
+function getGridColor(){
+    return gridColor;
+}
+
+function shade(){
+    return gridColor=colorPicker.value;
+}
+
+function decreaseOpacity(e){
+    if(e.style.opacity>=0){
+        e.style.opacity-=.1;
+    }
+}
 
 function clearGrid(){
     container.innerHTML="";
 }
-
-   
